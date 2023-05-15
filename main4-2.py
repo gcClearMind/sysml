e25276362353686dbdb7e26945a596dd06820cb2
shk314159 <50333579+shk314159@users.noreply.github.com>
2023-05-15 12:58:29 +0800

    5.15 get

--- main4-2.py
+++ main4-2.py
@@ -9,7 +9,6 @@ from Generalization import generalization
 CONFIG_FILE = 'config.ini'
 config = configparser.ConfigParser()
 config.read(CONFIG_FILE)
-# NoRead = config.get('NoRead', 'name').split()
 
 
 # 节点、关系列表
@@ -44,10 +43,10 @@ def get_map(collection):
 def get_Data(collection):
     Datas = collection.getElementsByTagName("uml:Model")
     for data in Datas:
-        print(data.nodeValue)
         return data
 
 
+NoRead_NodeName = config.get('NoRead', 'NodeName').split()
 NoRead_NodeType = config.get('NoRead', 'NodeType').split()
 
 
@@ -70,15 +69,16 @@ def dfs_isNode(node):
             dfs_isNode(child)
 
 
-def dfs_getAttribute(node, fatherNode=None):
+def dfs_getAttribute(node, fatherNodeId = None):
     # if node.attributes is None and not node.hasChildNodes():
     #     print(node)  # todo
     if node.nodeType == 3:
         return
     if node.hasAttribute("xmi:id") and 'href' not in node.attributes.keys():  # 原先是点
-        if node.getAttribute("xmi:type") in NoRead_NodeType:
+        if node.getAttribute("xmi:type") in NoRead_NodeType:  # 扩展和关系因为冗余性不读取
             return
         node_id = node.getAttribute("xmi:id")
+        # 属性读取
         for key in node.attributes.keys():
             if key == "xmi:id":
                 continue
@@ -88,18 +88,27 @@ def dfs_getAttribute(node, fatherNode=None):
                 RelationList.append(relation)
             else:
                 isNode[node_id][key] = value
-        if fatherNode is not None:  # 和上一个节点建立关系
-            pre_relation = Relationship(fatherNode, node.nodeName, isNode[node_id])
+        # 和上一个节点建立关系
+        if fatherNodeId is not None:
+            pre_relation = Relationship(isNode[fatherNodeId], node.nodeName, isNode[node_id])
             RelationList.append(pre_relation)
-
+        # 与儿子节点交互
         for child in node.childNodes:
-            dfs_getAttribute(child, isNode[node_id])
+            dfs_getAttribute(child, node_id)
 
-    else:
-        # if node.attributes is not None:
-
-        #     print()  # todo
-        print(node.nodeType)
+    else:   # 处理不是点的属性和链接
+        if node.nodeName in NoRead_NodeName or node.hasAttribute("href"):
+            return
+        if node.hasAttribute("xmi:idref") and node.getAttribute("xmi:idref") in isNode.keys():
+            relation = Relationship(isNode[fatherNodeId], node.nodeName, isNode[node.getAttribute("xmi:idref")])
+            RelationList.append(relation)
+        else:
+            for child in node.childNodes:
+                if child.nodeType == 3:  # 文本类
+                    if child.nodeValue.count("/n/t") != 0:
+                        continue
+                    # print(node.nodeName, child.nodeValue)
+                    isNode[fatherNodeId][node.nodeName] = child.nodeValue
 
 
 def test():
@@ -110,17 +119,22 @@ def test():
 
 
 def createNode(graph):
-    for node in NodeList:
-        graph.create(node)
+    for key in isNode.keys():
+        graph.create(isNode[key])
     for relation in RelationList:
         graph.create(relation)
 
 
-def find(Data):
-    package = Data.getElementsByTagName("packagedElement")
-    for e in package:
-        if e.getAttribute("xmi:id") == '_16_8_24400562_1521192267753_412535_14212':
-            generalization(e)
+def find(node):
+    if node.nodeType == 3:
+        return
+
+    if node.getAttribute("xmi:id") == '_16_8_24400562_1516347342392_171582_11816':
+        print(111)
+        for child in node.childNodes:
+            print(child)
+    for child in node.childNodes:
+        find(child)
 
 
 # 清空图
@@ -140,18 +154,19 @@ def create_graph(FILE_NAME):
     isNode[Data_id] = root_node
 
     dfs_isNode(Data)
+    # find(Data)
     dfs_getAttribute(Data)
-    # createRelation()
+
     # test()
     # # NEO4J链接
     # 本地
     # graph = Graph('bolt://localhost:7687', auth=('neo4j', '12345678'))
     # 服务器
-    # graph = Graph('bolt://121.40.183.82:7687', auth=('neo4j', '123456'))
+    graph = Graph('bolt://121.40.183.82:7687', auth=('neo4j', '123456'))
     #
-    # ClearGraph(graph)
-    # createNode(graph)
+    ClearGraph(graph)
+    createNode(graph)
 
 
 if __name__ == "__main__":
-    create_graph("MTSDesign_MobileRobot.xml")
+    create_graph("雷达软件系统.xml")
